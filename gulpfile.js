a427d8b2336d5a90f6d5d13f78efa3c698dcd0d1
Hyhello <673089899@qq.com>
2018-09-06 18:58:58 +0800

    修改打包配置

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,9 @@ var minifycss = require('gulp-minify-css');
 var uglify = require('gulp-uglify');
 var htmlmin = require('gulp-htmlmin');
 var htmlclean = require('gulp-htmlclean');
+var gutil = require('gulp-util');
+var babel = require('gulp-babel');
+
 // 压缩 public 目录 css
 gulp.task('minify-css', function() {
     return gulp.src('./public/**/*.css')
@@ -24,7 +27,13 @@ gulp.task('minify-html', function() {
 // 压缩 public/js 目录 js
 gulp.task('minify-js', function() {
     return gulp.src('./public/**/*.js')
+        .pipe(babel({
+            presets: ['es2015'] // es5检查机制
+        }))
         .pipe(uglify())
+        .on('error', function(err) {
+            gutil.log(gutil.colors.red('[Error]'), err.toString());
+        })
         .pipe(gulp.dest('./public'));
 });
 // 执行 gulp 命令时执行的任务
